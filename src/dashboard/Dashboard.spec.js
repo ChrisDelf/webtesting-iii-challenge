aa359c02a72be518bfcd9b4fa572ec6fc74caa2d
Chris-Delfaus <ChrisDelf@protonmail.com>
2019-08-19 17:06:17 -0400

    mvp finished

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,19 @@
-// Test away
+import React from 'react';
+import { toBeDisabled } from 'jest-dom';
+import renderer from 'react-test-renderer';
+import { render, fireEvent } from '@testing-library/react';
+import 'react-testing-library/cleanup-after-each';
+import Dashboard from './Dashboard';
+
+describe('Dasboard Tests', () => {
+  it('Does the Dashboard render', () => {
+    const { getByText } = render(<Dashboard />)
+
+    getByText(/open/i)
+    getByText(/unlocked/i)
+    getByText(/lock gate/i)
+    getByText(/close gate/i)
+
+
+  })
+})
