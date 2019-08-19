c9d7281d5cfcf40bc9decaa1c1bfad541c8ac8ad
Chris-Delfaus <ChrisDelf@protonmail.com>
2019-08-19 15:39:21 -0400

    initial commit got the one test working

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,19 @@
 // Test away!
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { render, fireEvent } from "@testing-library/react";
+
+import Controls from './Controls';
+
+describe ('<Controls>', () => {
+  it(' should match snapshot', () => {
+    const tree = renderer.create(<Controls />).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  })
+})
+
+
+
+
+
